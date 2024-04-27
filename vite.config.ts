fb1a6941eaea0806b80625e3444248516abf5627
Ondřej Čížek <ondrej.cizek@solidpixels.cz>
2024-04-27 19:27:23 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import fs from 'fs';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		https: {
-			key: fs.readFileSync(`${__dirname}/cert/key.pem`),
-			cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
-		}
-	}
+	plugins: [sveltekit(), mkcert()]
 });
